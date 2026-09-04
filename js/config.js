98d0d483bb8bc8cff283b55dc87e8c267dfe26dd
archiveghfreza-confsync <archive.ghfreza@gmail.com>
2026-09-04 23:49:58 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     REGISTER_API_URL:
-     'https://hooshteb-api.ghfreza.workers.dev/REGISTER_API_URL',
+        'https://hooshteb-api.ghfreza.workers.dev/api/REGISTER_API_URL',
 
     ASK_CONGRESS_API_URL:
-      'https://hooshteb-api.ghfreza.workers.dev/ASK_CONGRESS_API_URL',
-    CONGRESS_API_URL:
-        'https://hooshteb-api.ghfreza.workers.dev/CONGRESS_API_URL'
+        'https://hooshteb-api.ghfreza.workers.dev/api/ASK_CONGRESS_API_URL',
 
+    CONGRESS_API_URL:
+        'https://hooshteb-api.ghfreza.workers.dev/api/CONGRESS_API_URL'
 };
