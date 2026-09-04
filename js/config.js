b714325e9d7fc8ab78d932a802fa9a0f67faa4ec
archiveghfreza-confsync <archive.ghfreza@gmail.com>
2026-09-04 23:44:52 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     REGISTER_API_URL:
-     'https://script.google.com/macros/s/AKfycbycCl4EkZZbEGvBh8tGq90wwNiScyOnr_PM4un35-VWLKusx5kTz-pmxb1ilV70Ddkv/exec',
+     'https://hooshteb-api.ghfreza.workers.dev/REGISTER_API_URL',
 
     ASK_CONGRESS_API_URL:
-      'https://script.google.com/macros/s/AKfycbzDqRi95AIRVORJiOsiIT0RMOHWulmBDujyvKaRt-cKWEFn9Q5WXrb1-f3j2C3F_kL3/exec',
+      'https://hooshteb-api.ghfreza.workers.dev/ASK_CONGRESS_API_URL',
     CONGRESS_API_URL:
-        'https://script.google.com/macros/s/AKfycbwtyIaT_clo-0rnIsrjwWiBbYWMimo5jsInZw93rwfL50ifacsbS7YTMLDvdwWYGXHm5w/exec'
+        'https://hooshteb-api.ghfreza.workers.dev/CONGRESS_API_URL'
 
 };
